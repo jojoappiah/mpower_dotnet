e01b1b7703f95f5a107c77cee9eb5e066ec1ec4d
Jojo Appiah <jojo.appiah@live.co.uk>
2013-10-09 11:07:57 +0100

    Update MPowerUtility.cs
    Update replaces WebClient with HttpClient. using HttpClient gives better request control and also makes it fully Portable  (i.e can be used in Windows Phone, Windows 8/8.1 apps).
    (Download HttpClient libs from Nuget)

--- MPowerPayments/MPowerPayments/MPowerUtility.cs
+++ MPowerPayments/MPowerPayments/MPowerUtility.cs
@@ -1,8 +1,17 @@
+//MpowerPayments Utility Portable-lized , substituting this class with MpowerUtility class in your code will enable you to create a portable library for .NET
+//Portable class library will allow you to use this code in windows phone and windows 8/8.1 and silverlight applications 
+//Changes: Class modified to use HttpClient instead of WebClient
+//Author: Jojo Appiah
+//Date: 08-10-2013
+//Tested with : Windows Phone 8 and Windows 8 app
+
+
 using System;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Net;
 using System.Text;
+using System.Net.Http;
 
 namespace MPowerPayments
 {
@@ -13,43 +22,56 @@ namespace MPowerPayments
 		public MPowerUtility (MPowerSetup setup)
 		{
 			this.setup = setup;
+            
 		}
-
+        /// <summary>
+        //
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="payload"></param>
+        /// <returns></returns>
 		public JObject HttpPostJson (string url, string payload)
 		{
-			var bytes = Encoding.Default.GetBytes(payload);
 
-			WebClient client = new WebClient();
-			client.Headers.Add(HttpRequestHeader.UserAgent,
-			       "MPower Checkout API .NET client v1 aka Don Nigalon");
-
-			client.Headers.Add("mp-master-key", setup.MasterKey);
-			client.Headers.Add("mp-private-key", setup.PrivateKey);
-			client.Headers.Add("mp-public-key", setup.PublicKey);
-			client.Headers.Add("mp-token", setup.Token);
-			client.Headers.Add("mp-mode", setup.Mode);
-			client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
-
-			var response = client.UploadData(url, "POST", bytes);
-			
-			return JObject.Parse(Encoding.Default.GetString(response));
+            //seems the MPowerPayments API server does not support compression so this is currently disabled in code..  (re-enable the two lines below to support compression)
+           //    HttpClientHandler handler = new HttpClientHandler() { AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip };
+            //HttpClient client = new HttpClient(handler);       
+    
+		HttpClient client = new HttpClient();
+			client.DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue(new System.Net.Http.Headers.ProductHeaderValue("MPowerPortable","1.0")));
+			client.DefaultRequestHeaders.Add("mp-master-key", setup.MasterKey);
+            		client.DefaultRequestHeaders.Add("mp-private-key", setup.PrivateKey);
+            		client.DefaultRequestHeaders.Add("mp-public-key", setup.PublicKey);
+            		client.DefaultRequestHeaders.Add("mp-token", setup.Token);
+            		client.DefaultRequestHeaders.Add("mp-mode", setup.Mode);
+            		client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            
+		var response = client.PostAsync(new Uri(url), new System.Net.Http.StringContent(payload));
+         
+                var resp=response.Result.EnsureSuccessStatusCode();
+                return JObject.Parse(resp.Content.ReadAsStringAsync().Result);   
+           
 		}
 
 		public JObject HttpGetRequest(string url)
-		{
-			WebClient client = new WebClient();
-			client.Headers.Add(HttpRequestHeader.UserAgent,
-			       "MPower Checkout API .NET client v1 aka Don Nigalon");
-			
-			client.Headers.Add("mp-master-key", setup.MasterKey);
-			client.Headers.Add("mp-private-key", setup.PrivateKey);
-			client.Headers.Add("mp-public-key", setup.PublicKey);
-			client.Headers.Add("mp-token", setup.Token);
-			client.Headers.Add("mp-mode", setup.Mode);
+{
+    //seems the MPowerPayments API server does not support compression so this is currently disabled in code..  (re-enable the two lines below to support compression)
+    //    HttpClientHandler handler = new HttpClientHandler() { AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip };
+    //HttpClient client = new HttpClient(handler);       
+        
+			HttpClient client = new HttpClient();
+                        client.DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue(new System.Net.Http.Headers.ProductHeaderValue("MPowerPortable", "1.0")));
+                        client.DefaultRequestHeaders.Add("mp-master-key", setup.MasterKey);
+        		client.DefaultRequestHeaders.Add("mp-private-key", setup.PrivateKey);
+            		client.DefaultRequestHeaders.Add("mp-public-key", setup.PublicKey);
+            		client.DefaultRequestHeaders.Add("mp-token", setup.Token);
+            		client.DefaultRequestHeaders.Add("mp-mode", setup.Mode);
+            		client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
 
-			var response = client.DownloadString(url);
-
-			return JObject.Parse(response);
+			var response = client.GetStringAsync(url);
+            
+            		var rep = response.Result;
+			return JObject.Parse(rep);
 		}
 
 		public JObject ParseJSON(object jsontext)
